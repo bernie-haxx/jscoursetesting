d4811c560e403cbdfb72ec5c1efe8efee4aa911e
bernie-haxx <benkaranja43@gmail.com>
2024-11-04 09:25:47 -0500

    Reset button creation

--- followerCount/followers_count.js
+++ followerCount/followers_count.js
@@ -3,7 +3,7 @@ let count = 0; // Initialize count to 0
 function increaseCount() {
     count++; // Increment the count by 1
     displayCount(); // Display the count
-    checkCountValue() // Check count value and display message
+    checkCountValue() // Check count value and display messages
 }
 
 function displayCount() {
@@ -16,4 +16,11 @@ function checkCountValue () {
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
     }
+}
+
+function resetCount() {
+    count = 0;
+    document.getElementById('countDisplay').innerHTML=count;
+    alert(`The follower count has been reset to ${count}`);
+    
 }
